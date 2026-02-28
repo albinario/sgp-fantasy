2746bd868be3d9672fc73415e8d28892606554fc
Albin Lindeborg <albin.lindeborg@dynamicdog.se>
2026-02-28 01:13:33 +0100

    adjust type

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -1,13 +1,13 @@
 type TMessage = {
-	content: string
-	label: string
+	content?: string
+	label?: string
 }
 
 export function Message({ content, label }: TMessage) {
 	return (
 		<main>
-			<h1>{label}</h1>
-			<p>{content}</p>
+			{label ? <h1>{label}</h1> : null}
+			{content ? <p>{content}</p> : null}
 		</main>
 	)
 }
